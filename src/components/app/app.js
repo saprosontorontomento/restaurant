fd8d53d1a2ac5de2aa751f8e21c7842413658d05
econom1st <camcycamovu4@gmail.com>
2021-06-20 14:20:27 +0300

    add service and route

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import {MainPage, CartPage} from '../pages';
 import AppHeader from '../app-header';
+import { Route, Switch } from 'react-router-dom';
 
 import Background from './food-bg.jpg';
 
 const App = () => {
+
     return (
-        <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
-            <AppHeader total={50}/>
-            <MainPage/>
-            <CartPage/>
-        </div>
+    <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
+        <AppHeader total={50}/>
+        <Switch>
+            <Route 
+                path='/' 
+                component={MainPage}
+                exact/>
+            <Route 
+                path='/cart'  
+                component={CartPage}/>
+        </Switch>
+    </div>
     )
 }
 
--- src/components/hoc/with-resto-service.js
+++ src/components/hoc/with-resto-service.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import RestoServiceContext from '../resto-service-context';
-
-const WithRestoService = () => () => {
-    return 1;
+// сервис принимает какой-то другой компонент
+const WithRestoService = () => (Wrapped) => { // обозвали Wrapped
+    return (props) => { // в него передаются пропсы
+        return ( // рендерим такую структуру, где прячем consumer
+            <RestoServiceContext.Consumer>
+                {
+                    (RestoService) => {
+                        return <Wrapped {...props} RestoService={RestoService} />
+                    }
+                }
+            </RestoServiceContext.Consumer>
+        )
+    }
 };
 
 export default WithRestoService;
