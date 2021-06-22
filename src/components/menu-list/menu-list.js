59a1d4608a25711a600701c354cbb3d9b904bde4
econom1st <camcycamovu4@gmail.com>
2021-06-22 20:37:11 +0300

    add shopping basket

--- src/components/menu-list/menu-list.js
+++ src/components/menu-list/menu-list.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import MenuListItem from '../menu-list-item';
 import { connect } from 'react-redux'; // связываем menuList с redux
 import WithRestoService from '../hoc'; // записываем данные из компонента в наш state
-import { menuLoaded, menuRequested, menuError } from '../../actions';
+import { menuLoaded, menuRequested, menuError, addedToCart } from '../../actions';
 import Spinner from '../spinner';
 import Error from '../error';
 
@@ -21,7 +21,7 @@ class MenuList extends Component {
         }
 
     render() {
-        const {menuItems, loading, error} = this.props;
+        const {menuItems, loading, error, addedToCart} = this.props;
 
         if (error) {
             return <Error/>
@@ -35,7 +35,10 @@ class MenuList extends Component {
             <ul className="menu__list">
                 {
                     menuItems.map(menuItem => {
-                        return <MenuListItem key={menuItem.id} menuItem={menuItem} />
+                        return <MenuListItem 
+                                key={menuItem.id} 
+                                menuItem={menuItem}
+                                onAddToCart={() => addedToCart(menuItem.id)} />
                     })
                 }
             </ul>
@@ -54,7 +57,8 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = {
     menuLoaded,
     menuRequested,
-    menuError
+    menuError,
+    addedToCart
 };
 
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 const initialState = {
     menu: [],
     loading: true,
-    error: false
+    error: false,
+    items: []
 }
 
 const reducer = (state = initialState, action) => {
@@ -9,21 +10,52 @@ const reducer = (state = initialState, action) => {
     switch (action.type) {
         case 'MENU_LOADED':
             return {
+                ...state,
                 menu: action.payload,
                 loading: false
             };
 
         case 'MENU_REQUESTED':
             return {
+                ...state,
                 menu: state.menu,
                 loading: true
             }
 
         case 'MENU_ERROR':
             return {
+                ...state,
                 menu: state.menu,
                 loading: state.loading,
                 error: true
+            }
+
+        case 'ITEM_ADD_TO_CART':
+            const id = action.payload;
+            const item = state.menu.find(item => item.id === id);
+            const newItem = {
+                title: item.title,
+                price: item.price,
+                url: item.url,
+                id: item.id
+            }; 
+
+            return {
+                ...state,
+                items: [
+                    ...state.items,
+                    newItem
+                ]
+            };
+
+        case 'ITEM_REMOVE_FROM_CART':
+            const index = action.payload;
+            const newItems = state.items.filter(item => item.id !== index);
+            return {
+                ...state,
+                items: [
+                    ...newItems
+                ]
             }
         
         default:
